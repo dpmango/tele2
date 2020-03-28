2396556f9548a078375a5de5fee1df4987cc234c
Sergey Khmelevskoy <xs290@me.com>
2020-03-28 23:14:43 +0300

    select tarif, remove gsap

--- src/js/modules/cliks.js
+++ src/js/modules/cliks.js
@@ -8,53 +8,50 @@
         .on('click', '[href="#"]', function(e) {
           e.preventDefault();
         })
-        // scroll to section
-        .on('click', 'a[href^="#section"]', function() {
-          // section scroll
-          var el = $(this).attr('href');
-          var topTarget = $(el).offset().top;
+        // accardeon
+        .on('click', '.js-accardeon .accardeon__toggler', function(e) {
+          var $title = $(this);
+          var $element = $title.parent();
+          var $content = $element.find('.accardeon__content');
 
-          // $('body, html').animate({scrollTop: topTarget}, 1000);
-          TweenLite.to(window, 1, {
-            scrollTo: { y: topTarget, autoKill: false },
-            ease: easingSwing,
-          });
+          // clear previous active element(s)
+          // var $siblings = $element.siblings();
+          // if ($siblings.length > 0) {
+          //   $siblings.each(function(i, element) {
+          //     var $element = $(element);
+          //     var $content = $element.find('.accardeon__content');
 
+          //     if ($element.is('.is-active')) {
+          //       $element.removeClass('is-active');
+          //       $content.slideUp();
+          //     }
+          //   });
+          // }
+
+          // target current element
+          if ($element.is('.is-active')) {
+            $element.removeClass('is-active');
+            $content.slideUp();
+          } else {
+            $element.addClass('is-active');
+            $content.slideDown();
+          }
+        })
+
+        // accardeon
+        .on('click', '.js-select-tarif', function(e) {
+          // select by id in selectric
+          var id = $(this).data('id');
+          var $select = $('.form select');
+
+          $select.prop('selectedIndex', id).selectric('refresh');
+
+          // scroll to section
+          var topTarget = $('.form').offset().top;
+
+          $('body, html').animate({ scrollTop: topTarget }, 1000);
           return false;
         });
-
-      // accardeon
-      _document.on('click', '.js-accardeon .accardeon__toggler', function(e) {
-        var $title = $(this);
-        var $element = $title.parent();
-        var $content = $element.find('.accardeon__content');
-
-        // clear previous active element(s)
-        // var $siblings = $element.siblings();
-        // if ($siblings.length > 0) {
-        //   $siblings.each(function(i, element) {
-        //     var $element = $(element);
-        //     var $content = $element.find('.accardeon__content');
-
-        //     if ($element.is('.is-active')) {
-        //       $element.removeClass('is-active');
-        //       $content.slideUp();
-        //     }
-        //   });
-        // }
-
-        // target current element
-        if ($element.is('.is-active')) {
-          $element.removeClass('is-active');
-          $content.slideUp();
-        } else {
-          $element.addClass('is-active');
-          $content.slideDown();
-        }
-      });
-    },
-    destroy: function() {
-      // ... code ...
     },
   };
 })(jQuery, window.APP);
